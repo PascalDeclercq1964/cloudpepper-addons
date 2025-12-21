a8d5251375da07f3f2b08bbff6dddfa845a71670
PascalDeclercq1964 <40002391+PascalDeclercq1964@users.noreply.github.com>
2025-12-21 19:43:43 +0100

    .

--- website_price_hide_button/controllers/website_sale.py
+++ website_price_hide_button/controllers/website_sale.py
@@ -1,16 +1,18 @@
 from odoo import models, api
 from odoo.exceptions import ValidationError
 
+
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     @api.constrains('product_uom_qty')
     def _check_b2b_qty_multiple(self):
         for line in self:
+            # Alleen website orders
             if not line.order_id.website_id:
                 continue
 
-            # enkel B2B website
+            # Alleen B2B website (pas ID aan!)
             if line.order_id.website_id.id != 2:
                 continue
 
@@ -19,3 +21,16 @@ class SaleOrderLine(models.Model):
                 raise ValidationError(
                     f"Bestelhoeveelheid moet een veelvoud zijn van {min_qty}"
                 )
+
+    def _get_b2b_min_qty(self):
+        self.ensure_one()
+
+        pricelist = self.order_id.pricelist_id
+        product = self.product_id
+
+        price, rule = product._get_pricelist_price_rule(
+            pricelist,
+            self.product_uom_qty or 1
+        )
+
+        return rule.min_quantity if rule and rule.min_quantity else 1
