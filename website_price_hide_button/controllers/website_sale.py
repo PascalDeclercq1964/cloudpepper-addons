746363beef3e72e96923ba122eb6758e6470a8e0
PascalDeclercq1964 <40002391+PascalDeclercq1964@users.noreply.github.com>
2025-12-21 19:39:20 +0100

    .

--- website_price_hide_button/controllers/website_sale.py
+++ website_price_hide_button/controllers/website_sale.py
@@ -1,42 +1,21 @@
-# -*- coding: utf-8 -*-
-from odoo import http
-from odoo.http import request
-from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo import models, api
+from odoo.exceptions import ValidationError
 
-class WebsiteSaleInherit(WebsiteSale):
-    """Class to hide price and replace buy button with contact us"""
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
 
-    @http.route([
-        '''/shop''',
-        '''/shop/page/<int:page>''',
-        '''/shop/category/<model("product.public.category"):category>''',
-        '''/shop/category/<model("product.public.category"):category>/page/<int:page>'''
-    ], type='http', auth="public", website=True)
-    def shop(self, page=0, category=None, search='', min_price=0.0,
-             max_price=0.0, ppg=False, **post):
-        """Add login_user to context"""
-        res = super().shop(page, category, search, min_price,
-                           max_price, ppg, **post)
-        res.qcontext.update({
-            'login_user': request.env.user._is_public()
-        })
-        return res
+    @api.constrains('product_uom_qty')
+    def _check_b2b_qty_multiple(self):
+        for line in self:
+            if not line.order_id.website_id:
+                continue
 
-    def _prepare_product_values(self, product, category, search, **kwargs):
-        """Add login_user to product page context"""
-        res = super(WebsiteSaleInherit, self)._prepare_product_values(product,
-                                                                      category,
-                                                                      search,
-                                                                      **kwargs)
-        res['login_user'] = request.env.user._is_public()
-        return res
+            # enkel B2B website
+            if line.order_id.website_id.id != 2:
+                continue
 
-    @http.route()
-    def shop_payment(self, **post):
-        """Restrict public visitors from accessing payment page"""
-        user = http.request.env.user
-        if not user._is_public() and (user.has_group('base.group_portal') or 
-                                     user.has_group('base.group_user')):
-            res = super(WebsiteSaleInherit, self).shop_payment(**post)
-            return res
-        return request.redirect("/")
+            min_qty = line._get_b2b_min_qty()
+            if min_qty > 1 and line.product_uom_qty % min_qty != 0:
+                raise ValidationError(
+                    f"Bestelhoeveelheid moet een veelvoud zijn van {min_qty}"
+                )
