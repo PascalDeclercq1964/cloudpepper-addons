5c004a22098b3d4ad96a337cc0a52bc79df38d6f
PascalDeclercq1964 <40002391+PascalDeclercq1964@users.noreply.github.com>
2025-12-23 20:11:05 +0100

    .

--- website_price_hide_button/__manifest__.py
+++ website_price_hide_button/__manifest__.py
@@ -9,6 +9,10 @@
     'author': 'Akshar Group Technologies',
     'website': 'https://www.akshargrouptechnologies.com/',
     'depends': ['website_sale'],
+    'assets': {
+        'web.assets_frontend': [
+            'website_price_hide_button/static/src/js/quantity_step.js',
+        ],
     'images': ['static/description/hideprice.png'],
     "icon": "static/description/icon.png",
      'data': [
