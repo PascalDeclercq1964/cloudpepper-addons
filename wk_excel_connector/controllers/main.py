502d43f4109ff05061821f08898308172ea6a0fe
PascalDeclercq1964 <40002391+PascalDeclercq1964@users.noreply.github.com>
2026-01-07 16:26:00 +0100

    tweak #3

--- wk_excel_connector/controllers/main.py
+++ wk_excel_connector/controllers/main.py
@@ -84,18 +84,16 @@ class WkExcelController(http.Controller):
     def get_field_labels(self, template):
         field_keys, labels = [], []
         for field in template.field_ids:
-            # begin aanpassing
             key = field.field_id.name
             # AANPASSING: Als het veld een Many2one relatie is, vraag dan specifiek de ID op
             if field.field_id.ttype == 'many2one':
                 key += '/id'
-            #einde aanpassing    
             
             field_keys.insert(field.sequence, key)
             labels.insert(field.sequence, field.name)
 
-        return field_keys, labels
-    
+        return field_keys, labels    
+        
     def get_csv_data(self, data, labels):
       
         file = io.BytesIO()
